88a91fb5c854db56120c1be7dd2d90d7febdeda0
raprayoga26 <prayogarafiea@gmail.com>
2021-05-18 02:51:49 +0700

    create button component and test these

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
 function App() {
-  return <div className="App">HELLO WORLD</div>;
+  return (
+    <>
+      <div className="App">HELLO WORLD</div>
+      <Button variant="primary">Info</Button>
+    </>
+  );
 }
 
 export default App;
--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-export default function Button(props) {
-  return <div></div>;
-}
+import Button from "./index";
+
+test("Should not allowed click button if isDisabled is present", () => {
+  const { container } = render(<Button isDisabled></Button>);
+
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("Should render loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument;
+  expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("Should render <a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("Should render <Link> component", () => {
+  const { container } = render(
+    <Router>
+      <Button href="" type="link"></Button>
+    </Router>
+  );
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
