0dd6fa6f6f1072e133916902778e2ed4cb6d315a
cakrawalabahasa <officialcakrawalabahasa@gmail.com>
2021-06-15 22:40:18 +0700

    news detail

--- src/components/pages/newsDetail/NewsDetail.js
+++ src/components/pages/newsDetail/NewsDetail.js
@@ -2,8 +2,17 @@ import React, { Component } from "react";
 import Swal from "sweetalert2";
 import axios from "axios";
 
+import { Link } from "react-router-dom";
+import { faChevronRight, faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import Header from "components/parts/header/Header";
 import Footer from "components/parts/footer/Footer";
+
+import FB from "assets/images/newsDetail/facebook.png";
+import Share from "assets/images/newsDetail/share.png";
+import Twitter from "assets/images/newsDetail/twitter.png";
+import WA from "assets/images/newsDetail/whatsapp.png";
 
 import "components/pages/newsDetail/newsDetail.css";
 
@@ -13,6 +22,8 @@ export default class NewsDetail extends Component {
     this.state = {
       artikel: "",
       id: this.props.match.params.id,
+      dateUpload: "",
+      timeUpload: "",
     };
 
     this.Toast = Swal.mixin({
@@ -36,7 +47,13 @@ export default class NewsDetail extends Component {
     await axios
       .get(`http://127.0.0.1:8000/api/artikel/${this.state.id}`)
       .then((resp) => {
-        this.setState({ artikel: resp.data.artikel });
+        this.setState({
+          artikel: resp.data.artikel,
+          dateUpload: resp.data.artikel.created_at.split("T")[0],
+          timeUpload: resp.data.artikel.created_at
+            .split("T")[1]
+            .split(".000000Z")[0],
+        });
       })
       .catch((error) => {
         this.Toast.fire({
@@ -51,17 +68,92 @@ export default class NewsDetail extends Component {
       <>
         <Header {...this.props}></Header>
 
-        <div className="container custom-container">
-          <img
-            src={
-              "http://127.0.0.1:8080/img/artikel/" + this.state.artikel.image
-            }
-            className="img-fluid"
-            alt="Contact Us"
-          />
-          <div
-            dangerouslySetInnerHTML={{ __html: this.state.artikel.artikel }}
-          />
+        <div className="container mt-3">
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li>
+                <Link to="/">
+                  <FontAwesomeIcon icon={faHome} />
+                </Link>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faChevronRight} className="mx-3" />
+              </li>
+              <li className="breadcrumb-item" aria-current="page">
+                <Link to="/news">News</Link>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faChevronRight} className="mx-3" />
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                {this.state.artikel.judul}
+              </li>
+            </ol>
+          </nav>
+        </div>
+
+        <div className="container artikel-container">
+          <div className="row mb-3">
+            <h2>{this.state.artikel.judul}</h2>
+          </div>
+
+          <div className="row share-button">
+            <img src={Share} className="img-fluid" alt="share" />
+            <a
+              target="blank"
+              href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`}
+              class="fb-xfbml-parse-ignore"
+            >
+              <img src={FB} className="img-fluid" alt="facebook share" />
+            </a>
+            <a
+              target="blank"
+              href="https://twitter.com/share?ref_src=https://news.detik.com"
+              data-show-count="false"
+            >
+              <img src={Twitter} className="img-fluid" alt="twitter share" />
+            </a>
+            <img src={WA} className="img-fluid" alt="whatsapp share" />
+          </div>
+
+          <div className="row mt-4 mb-3">
+            <img
+              src={
+                "http://127.0.0.1:8080/img/artikel/" + this.state.artikel.image
+              }
+              className="img-fluid"
+              alt="Contact Us"
+            />
+          </div>
+          <p className="author my-0">By {this.state.artikel.author}</p>
+          <small className="font-size-sm">
+            {this.state.dateUpload} {this.state.timeUpload}
+          </small>
+
+          <div className="row my-3">
+            <div
+              dangerouslySetInnerHTML={{ __html: this.state.artikel.artikel }}
+            />
+          </div>
+
+          <div className="row share-button">
+            <img src={Share} className="img-fluid" alt="share" />
+            <a
+              target="blank"
+              href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`}
+              class="fb-xfbml-parse-ignore"
+            >
+              <img src={FB} className="img-fluid" alt="facebook share" />
+            </a>
+            <a
+              target="blank"
+              href="https://twitter.com/share?ref_src=https://news.detik.com"
+              data-show-count="false"
+            >
+              <img src={Twitter} className="img-fluid" alt="twitter share" />
+            </a>
+            <img src={WA} className="img-fluid" alt="whatsapp share" />
+          </div>
         </div>
 
         <Footer {...this.props}></Footer>
